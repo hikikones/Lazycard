5492b4bf29648205bf9fcee679b852e05c7e0c40
hikikones <hikikones@gmail.com>
2019-12-06 12:36:32 +0100

    New project structure for complete rewrite

---
+++ src/main/main.ts
@@ -0,0 +1,39 @@
+import { app, BrowserWindow } from "electron";
+
+const isDev = process.env.NODE_ENV === "development";
+
+let mainWindow: Electron.BrowserWindow;
+
+function createWindow() {
+	mainWindow = new BrowserWindow({
+		height: 600,
+		width: 800,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+
+	mainWindow.loadFile("index.html");
+
+	if (isDev) {
+		mainWindow.webContents.openDevTools();
+	}
+
+	mainWindow.on("closed", () => {
+		mainWindow = null;
+	});
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+app.on("activate", () => {
+	if (mainWindow === null) {
+		createWindow();
+	}
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,38 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const nodeExternals = require("webpack-node-externals");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const BUILD_DIR = path.resolve(__dirname, "build");
-const APP_DIR = path.resolve(__dirname, "src");
+const BUILD_DIR = __dirname + "/build";
+const APP_DIR = __dirname + "/src";
 
-const jsRule = {
-  test: /\.(jsx?)$/,
-  exclude: /node_modules/,
-  use: {
-    loader: "babel-loader",
-    options: {
-      presets: ["@babel/preset-env", "@babel/preset-react"]
-    }
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  }
+const tsRule = {
+    test: /\.ts(x?)$/,
+    use: 'ts-loader'
 };
-
-const htmlRule = {
-  test: /\.html$/,
-  use: "html-loader"
-};
-
-const cssRule = {
-  test: /\.css$/,
-  use: ["style-loader", "css-loader"]
-};
-
-const fileRule = {
-  test: /\.(png|jpe?g|gif|ttf|woff2?|eot)$/,
-  use: "file-loader"
-};
-
-const rawRule = {
-  test: /\.(txt|sql)$/i,
-  use: "raw-loader"
-};
-
-const htmlPlugin = new HtmlWebPackPlugin({
-  template: APP_DIR + "/index.html",
-  filename: "index.html"
-});
-
-const nodeSettings = {
-  __dirname: false,
-  __filename: false
-};
-
-const externalsList = [nodeExternals()];
 
 const mainConfig = {
-  target: "electron-main",
-  entry: APP_DIR + "/main.js",
-  output: {
-    filename: "main.js",
-    path: BUILD_DIR
-  },
-  module: {
-    rules: [jsRule, fileRule, rawRule]
-  },
-  node: nodeSettings,
-  externals: externalsList
+    target: 'electron-main',
+    entry: APP_DIR + "/main/main.ts",
+    output: {
+        path: BUILD_DIR,
+        filename: "main.js"
+    },
+    module: {
+        rules: [tsRule]
+    }
 };
 
 const rendererConfig = {
-  target: "electron-renderer",
-  entry: APP_DIR + "/renderer.js",
-  output: {
-    filename: "renderer.js",
-    path: BUILD_DIR
-  },
-  module: {
-    rules: [jsRule, htmlRule, cssRule, fileRule, rawRule]
-  },
-  plugins: [htmlPlugin],
-  node: nodeSettings,
-  externals: externalsList
+    target: 'electron-renderer',
+    entry: APP_DIR + "/renderer/index.tsx",
+    output: {
+        path: BUILD_DIR,
+        filename: "index.js"
+    },
+    module: {
+        rules: [tsRule]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: APP_DIR + "/renderer/index.html"})
+    ]
 };
 
 module.exports = [mainConfig, rendererConfig];
