ddb8e04e78a0afab4842f4da518a41e906735503
hikikones <hikikones@gmail.com>
2019-12-12 01:41:09 +0100

    Added new nav links and changed nav items to function components

--- src/renderer/components/Nav.tsx
+++ src/renderer/components/Nav.tsx
@@ -15,15 +15,28 @@ export default class Nav extends React.Component<INavProps> {
         this.props.onTopicChange();
     }
 
+    private import = () => {
+        // TODO: db.import?
+        console.log("IMPORT");
+    }
+
     public render() {
         return (
             <nav>
                 <NavSection title="Cards">
-                    <NavItem name="All cards" to="/cards/" />
+                    <NavSectionLink name="All cards" to="/cards/" />
                 </NavSection>
+
+                <NavSection title="Database">
+                    <NavSectionLink name="Settings" to="/settings" />
+                    <NavSectionButton name="Import" action={this.import} />
+                </NavSection>
+
                 <NavSection title="Topics">
-                    {this.props.topics.map(t => <NavItem key={t.id} name={t.name} to={`/topics/${t.id}`} />)}
-                    <button onClick={this.addTopic}>Add</button>
+                    {this.props.topics.map(t =>
+                        <NavSectionLink key={t.id} name={t.name} to={`/topics/${t.id}`} />
+                    )}
+                    <NavSectionButton name="Add topic" action={this.addTopic} />
                 </NavSection>
             </nav>
         );
@@ -35,43 +48,29 @@ interface INavProps {
     onTopicChange(): void
 }
 
-class NavSection extends React.Component<INavSectionProps> {
-    public constructor(props: INavSectionProps) {
-        super(props);
-    }
-
-    public render() {
-        return (
-            <section>
-                <h4>{this.props.title}</h4>
-                <ul>
-                    {this.props.children}
-                </ul>
-            </section>
-        )
-    }
+const NavSection = (props: { title: string, children: React.ReactNode }) => {
+    return (
+        <section>
+            <h4>{props.title}</h4>
+            <ul>
+                {props.children}
+            </ul>
+        </section>
+    )
 }
 
-interface INavSectionProps {
-    title: string
-    children: React.ReactNode
+const NavSectionLink = (props: { name: string, to: string }) => {
+    return (
+        <li>
+            <NavLink exact to={props.to}>{props.name}</NavLink>
+        </li>
+    )
 }
 
-class NavItem extends React.Component<INavSectionItem> {
-    public constructor(props: INavSectionItem) {
-        super(props);
-    }
-
-    public render() {
-        return (
-            <li>
-                <NavLink exact to={this.props.to}>{this.props.name}</NavLink>
-            </li>
-        )
-    }
-}
-
-interface INavSectionItem {
-    name: string
-    to: string
+const NavSectionButton = (props: { name: string, action(): void }) => {
+    return (
+        <li>
+            <a href="#" onClick={props.action}>{props.name}</a>
+        </li>
+    )
 }
