8111d9215bf589732ca0770b38522685358f3fc0
Olav Kåre Vatne <olavvatne@gmail.com>
2019-12-28 12:18:21 +0100

    Set up CI with Azure Pipelines [skip ci]

---
+++ test/app.test.ts
@@ -0,0 +1,11 @@
+
+import { expect } from "chai";
+import "mocha";
+
+describe("App", () => {
+  describe("should", () => {
+    it("always pass", () => {
+      expect(true).to.equal(true);
+    });
+  });
+});
