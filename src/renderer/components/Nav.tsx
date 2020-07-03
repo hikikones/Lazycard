c65d30463dea08c5b9f69d98e4f833685c70cd92
hikikones <hikikones@gmail.com>
2020-07-03 14:57:00 +0200

    NavLink now matches IDs

--- src/renderer/components/Nav.tsx
+++ src/renderer/components/Nav.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { NavLink as ReactRouterNavLink } from "react-router-dom";
 
-// TODO: Fix NavLink to '/review/0' so that all topicIds are matched, not only just 0
-
 const Nav = () => {
     return (
         <nav>
-            <NavLink to="/review/0" icon="drafts" />
+            <NavLinkWithId to="/review/0" icon="drafts" />
             <NavLink to="/cards" icon="layers" />
             <NavLink to="/topics" icon="dashboard" />
             <NavLink to="/settings" icon="settings" />
@@ -16,7 +14,23 @@ const Nav = () => {
 
 const NavLink = (props: {to: string, icon: string}) => {
     return (
-        <ReactRouterNavLink exact to={props.to} className="nav">
+        <ReactRouterNavLink to={props.to} className="nav">
+            <i className="material-icons icon">{props.icon}</i>
+        </ReactRouterNavLink>
+    );
+}
+
+const NavLinkWithId = (props: {to: string, icon: string}) => {
+    return (
+        <ReactRouterNavLink
+            to={props.to}
+            className="nav"
+            isActive={(match, location) => {
+                const tokens = location.pathname.split('/');
+                const id = tokens[tokens.length - 1];
+                return !isNaN(Number(id));
+            }}
+        >
             <i className="material-icons icon">{props.icon}</i>
         </ReactRouterNavLink>
     );
