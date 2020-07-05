9d633b24e60ee8b820a3a573ec061021bb3d7a60
hikikones <hikikones@gmail.com>
2020-07-05 23:09:36 +0200

    Only render backside of card when there is content

--- src/renderer/components/CardView.tsx
+++ src/renderer/components/CardView.tsx
@@ -3,11 +3,22 @@ import * as React from 'react';
 import md from '../controller/Markdown';
 
 const Card = (props: ICardViewProps) => {
+    const back = () => {
+        if (!props.showBack || props.back === "")
+            return null;
+
+        return (
+            <div>
+                <hr />
+                <CardContent markdown={props.back} />
+            </div>
+        );
+    }
+
     return (
         <div className="card shadow">
             <CardContent markdown={props.front} />
-            {props.showBack && <hr />}
-            {props.showBack && <CardContent markdown={props.back} />}
+            {back()}
 
             {props.children || null}
         </div>
