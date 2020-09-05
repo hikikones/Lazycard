f2755b92e89c479d658e5f2498657ab4a011fa33
nepothar <nepothar@microsoft.com>
2020-09-05 15:06:23 -0700

    added countdown timer to card

--- src/renderer/components/Card.tsx
+++ src/renderer/components/Card.tsx
@@ -7,6 +7,7 @@ import CardView from './CardView';
 import Dropdown, { DropdownItem } from './Dropdown';
 import Modal from './Modal';
 import CardEditor from './CardEditor';
+import TimerSimple from './TimerSimple';
 
 const Card = (props: ICardProps) => {
     const [showEditor, setShowEditor] = React.useState<boolean>(false);
@@ -27,6 +28,11 @@ const Card = (props: ICardProps) => {
         props.onDelete();
     }
 
+    const startTimer = () => {
+        setShowStats(show => !show);
+        if (props.onToggleModal !== undefined) props.onToggleModal();
+    }
+
     return (
         <>
             <CardView front={props.card.front} back={props.card.back} showBack={props.showBack}>
@@ -35,7 +41,7 @@ const Card = (props: ICardProps) => {
                     <DropdownItem name="Stats" icon="assessment" action={toggleStats} />
                     <DropdownItem name="Delete" icon="delete" action={onDelete} />
                 </Dropdown>
-
+                <TimerSimple timerEnabled={false} seconds={10} minutes={1}></TimerSimple>
                 {props.children || null}
             </CardView>
 
