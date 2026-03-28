54be075320ad2879ab336cb8224fc59dda18ca9c
hikikones <hikikones@gmail.com>
2026-03-28 10:40:44 +0100

    add clap styling

--- lazycard/src/main.rs
+++ lazycard/src/main.rs
@@ -1,15 +1,10 @@
-use clap::Parser;
-
 mod app;
 mod pages;
 mod terminal;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
-    let db = database::Database::new(
-        std::path::PathBuf::from(args.database),
-        args.desired_retention as f32 / 100.0,
-    )?;
+    let args: Args = clap::Parser::parse();
+    let db = database::Database::new(args.database, args.desired_retention as f32 / 100.0)?;
 
     let terminal = terminal::Terminal::init()?;
 
@@ -22,13 +17,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     res
 }
 
-/// A flashcard application for the terminal
-#[derive(Debug, Parser)]
-#[command(version, about, long_about = None)]
+#[derive(Debug, clap::Parser)]
+#[command(version, about, styles = CLAP_STYLING)]
 struct Args {
     /// Where to store your cards [example: ~/lazycard.ron]
-    #[arg(value_name = "DATABASE_FILE", value_hint = clap::ValueHint::FilePath)]
-    database: String,
+    #[arg(value_name = "DATABASE_FILE.ron", value_hint = clap::ValueHint::FilePath)]
+    database: std::path::PathBuf,
 
     /// Desired retention in percent for your cards
     #[arg(long, value_name = "PERCENT", default_value_t = 80, value_parser = clap::value_parser!(u8).range(0..=100))]
@@ -38,3 +32,12 @@ struct Args {
     #[clap(long, action)]
     external_editor: bool,
 }
+
+const CLAP_STYLING: clap::builder::styling::Styles = clap::builder::styling::Styles::styled()
+    .header(clap_cargo::style::HEADER)
+    .usage(clap_cargo::style::USAGE)
+    .literal(clap_cargo::style::LITERAL)
+    .placeholder(clap_cargo::style::PLACEHOLDER)
+    .error(clap_cargo::style::ERROR)
+    .valid(clap_cargo::style::VALID)
+    .invalid(clap_cargo::style::INVALID);
