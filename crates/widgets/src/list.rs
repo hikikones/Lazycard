c6319f878866999c9f1216526b80bc54363f6e66
hikikones <hikikones@gmail.com>
2026-07-05 17:05:11 +0200

    add todo

--- crates/widgets/src/list.rs
+++ crates/widgets/src/list.rs
@@ -6,6 +6,8 @@ use ratatui::{
 
 use crate::{Scrollbar, ScrollbarColors};
 
+// TODO: Add render_with_splits or with_splits for horizontal split of the area.
+
 pub struct List {
     index: usize,
     selector: Option<usize>,
